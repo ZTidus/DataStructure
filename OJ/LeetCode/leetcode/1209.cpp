8210e382524b4ac90249d0fa226d6a23ffae21b5
ongoing-z <1413677817@qq.com>
2020-08-10 22:00:52 +0800

    add Stack - 1209

---
+++ OJ/LeetCode/leetcode/1209.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+/* Leetcode */
+/* 题目信息 */
+/*
+ *1209. Remove All Adjacent Duplicates in String II
+Given a string s, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made.
+
+It is guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+ 
+
+Constraints:
+
+1 <= s.length <= 10^5
+2 <= k <= 10^4
+s only contains lower case English letters.
+ */
+
+/* my solution */
+// none
+
+/* better solution */
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        vector<pair<char, int>> vec;
+        
+        for (char& c: s)
+        {
+            if (vec.empty())
+                vec.push_back(make_pair(c, 1));
+            else if (c == vec.back().first && vec.back().second == k-1)
+                vec.pop_back();
+            else if (c == vec.back().first)
+                vec.back().second++;
+            else 
+                vec.push_back(make_pair(c, 1));
+        }
+        
+        string res;
+        for (auto &p: vec)
+        {
+            for (int i = 0; i < p.second; i++)
+                res += p.first;
+        }
+        
+        return res;
+    }
+};
+
+/* 一些总结 */
+// 1. make_pair()
+
