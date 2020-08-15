7950c6fe2a5f1b3f4a2dccad66be4ac9caca0025
ongoing-z <1413677817@qq.com>
2020-08-15 16:29:35 +0800

    add tree - 104

---
+++ OJ/LeetCode/leetcode/104.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/* Leetcode */
+/* 题目信息 */
+/*
+ *
+ */
+
+/* my solution */
+// ✓  easy to understand
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+    void dfs(TreeNode* root, int level)
+    {
+        if (!root) return;
+        if (!root->left && !root->right)
+        {
+            if (level > res)
+                res = level;
+            return;
+        }
+        
+        if (root->left)
+            dfs(root->left, level + 1);
+        if (root->right)
+            dfs(root->right, level + 1);
+    }
+    
+    int maxDepth(TreeNode* root) {
+        dfs(root, 1);
+        
+        return res;
+    }
+};
+
+/* better solution */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        int max_depth = 1;
+        if (root->left)
+            max_depth = max(max_depth, 1 + maxDepth(root->left));
+        if (root->right)
+            max_depth = max(max_depth, 1 + maxDepth(root->right));
+            
+        return max_depth;
+    }
+};
+
+/* 一些总结 */
+// 1. 题意: 
+//
+// 需要注意的点: 
+// 1. 下面一种方法写法更加简洁。
+// 2. 
+// 3. 
+
