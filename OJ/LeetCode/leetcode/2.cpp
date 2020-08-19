67b5690191f168c6de39c8fca09cafbac6e9e05c
ongoing-z <1413677817@qq.com>
2020-08-19 20:04:29 +0800

    add a new way and comment

--- OJ/LeetCode/leetcode/2.cpp
+++ OJ/LeetCode/leetcode/2.cpp
@@ -61,5 +61,49 @@ public:
     }
 };
 
-/* 一些总结 */
+// 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        
+        int carry = 0;
+        while (l1 || l2)
+        {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            if (sum >= 10)
+            {
+                sum -= 10;
+                carry = 1;
+            }
+            else
+                carry = 0;
+            tail->next = new ListNode(sum);
+            tail = tail->next;
+            
+            //if (l1) l1 = l1->next;
+            //if (l2) l2 = l2->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+        }
+        if (carry)
+            tail->next = new ListNode(carry);
+        
+        return dummy->next;
+    }
+};
 
+/* 一些总结 */
+// 1. dummy head
+// 2. l1 = l1 ? l1->next : nullptr;
