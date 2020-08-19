53fd1c1ef88576e5930112cd5fd71d46307200d7
ongoing-z <1413677817@qq.com>
2020-08-19 11:01:23 +0800

    add a new way and answer a question

--- OJ/LeetCode/leetcode/108.cpp
+++ OJ/LeetCode/leetcode/108.cpp
@@ -27,6 +27,7 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
 // none
 
 /* better solution */
+// 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -56,8 +57,39 @@ public:
     }
 };
 
+// 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+private:
+    TreeNode* helper(vector<int> nums, int L, int R)
+    {
+        if (L > R) return nullptr;
+        int mid = L + (R - L) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = helper(nums, L, mid - 1);
+        root->right = helper(nums, mid + 1, R);
+        
+        return root;
+    }
+};
+
 /* 一些总结 */
 // 1. 使用递归，递归边界: end <= start
 // 2. 递归式: 找出中间索引
 // 3. return toBST(nums, 0, nums.size());
 // 这里的nums.size()为什么不是num.size()-1???
+// 注意第二种方法，是对上一个问题的回答。
