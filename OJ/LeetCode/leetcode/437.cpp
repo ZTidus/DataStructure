9729fd4a8f6c1fe9ae94da197e4168ab51a18fb1
ongoing-z <1413677817@qq.com>
2020-08-31 23:19:44 +0800

    add a new way

--- OJ/LeetCode/leetcode/437.cpp
+++ OJ/LeetCode/leetcode/437.cpp
@@ -36,7 +36,43 @@ Return 3. The paths that sum to 8 are:
  */
 
 /* my solution */
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if (!root) return 0;
+        int self = helper(root, sum);
+        int L = helper(root->left, sum);
+        int R = helper(root->right, sum);
+        
+        return L + R + self;
+    }
+private:
+    int helper(TreeNode* root, int sum)
+    {
+        if (!root) return 0;
+        int self;
+        if (root->val == sum)
+            self = 1;
+        else 
+            self = 0;
+        int L = helper(root->left, sum - root->val);
+        int R = helper(root->right, sum - root->val);
+        
+        return L + R + self;
+    }
+    
+};
 
 /* better solution */
 class Solution {
