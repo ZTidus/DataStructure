9d9f0ed2117d60e5d2f43ede89f44b03074e07e5
ongoing-z <1413677817@qq.com>
2020-11-23 20:33:10 +0800

    add sql - 1527

---
+++ OJ/LeetCode/leetcode/1527.sql
@@ -0,0 +1,24 @@
+/* Leetcode-cn  */
+/* Type: sql */
+/* 题目信息 */
+/*
+ *1527. 患某种疾病的患者
+ */
+
+/* my solution */
+-- solution-x, ms, defeat %
+
+
+/* better solution */
+-- solution-x, ms, defeat %
+select * from Patients
+where conditions like "%DIAB1%";
+
+/* 一些总结 */
+-- 1. 题意: 很简单的like题目
+--
+-- 需要注意的点: 
+-- 1. 
+-- 2. 
+-- 3. 
+
