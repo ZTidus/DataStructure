e997b68fd5f9659c58dedfbc3fb475a9513534b1
ongoing-z <1413677817@qq.com>
2020-12-14 18:01:52 +0800

    add array - 1672

---
+++ OJ/LeetCode/leetcode/1672.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <set>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+/* Leetcode */
+/* Type: very easy */
+/* 题目信息 */
+/*
+ *1672. Richest Customer Wealth
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+ 
+
+Example 1:
+
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+Example 2:
+
+Input: accounts = [[1,5],[7,3],[3,5]]
+Output: 10
+Explanation: 
+1st customer has wealth = 6
+2nd customer has wealth = 10 
+3rd customer has wealth = 8
+The 2nd customer is the richest with a wealth of 10.
+Example 3:
+
+Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+Output: 17
+ 
+
+Constraints:
+
+m == accounts.length
+n == accounts[i].length
+1 <= m, n <= 50
+1 <= accounts[i][j] <= 100
+ */
+
+/* my solution */
+// solution-x, ms, defeat %
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        // compute each line
+        vector<int> money;
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].size(); j++) {
+                sum += accounts[i][j];
+            }
+            money.push_back(sum);
+        }
+        sort(money.begin(), money.end());
+        return money.back();
+        
+    }
+};
+
+/* better solution */
+// solution-x, ms, defeat %
+
+
+/* 一些总结 */
+// 1. 题意: 计算二维矩阵中每一行的和.
+//
+// 需要注意的点: 
+// 1. 
+// 2. 
+// 3. 
+
+/*
+ * main test
+ */
+
+int main(void)
+{
+    Solution solution;
+
+    return 0;
+}
+
