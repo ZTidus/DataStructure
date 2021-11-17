ee8a2db1a288a954131e01a334fe1c483bfdefa2
ongoing-z <1413677817@qq.com>
2021-11-17 22:36:16 +0800

    list(easy) - 1431

---
+++ OJ/LeetCode/leetcode/problems/1431.java
@@ -0,0 +1,49 @@
+// Question: 1431. Kids With the Greatest Number of Candies
+
+// Meaning of question
+// Compute if array[i] add a number will be the greatest number of this array.
+
+// ------------------------------ code ------------------------------
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> res = new ArrayList<>();
+
+        int maxV = Arrays.stream(candies).max().getAsInt();
+
+        for (int i = 0; i < candies.length; i++) {
+            if ((candies[i] + extraCandies) >= maxV) {
+                res[i] = true;
+            } else {
+                res[i] = false;
+            }
+        }
+
+        return res;
+    }
+}
+
+// RunTime: 2ms
+
+// ------------------------------ code ------------------------------
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        int maxV = -1;
+        
+        // compute max
+        for (int i = 0; i < candies.length; i++) {
+            maxV = Math.max(candies[i], maxV);
+        }
+        
+        List<Boolean> res = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            res.add((candies[i] + extraCandies) >= maxV);
+        }
+        return res;
+    }
+}
+
+// RunTime: 1ms
+
